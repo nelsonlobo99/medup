37e65b1b922cd45fa6821a3eae2583d866c131a9
nelsonlobo99 <nelsonlobo@protonmail.com>
2020-07-25 22:29:28 +0530

    Add MedUp

---
+++ medup_client/android/app/src/main/kotlin/com/example/medup_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medup_client
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ medup_doc/android/app/src/main/kotlin/com/example/medup_doc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.medup_doc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
